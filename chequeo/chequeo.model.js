3eb73097c822f754bc7c5cfda7e487dc0cd1eb46
Nicolas Bernal <nicolasbernal105@gmail.com>
2024-05-26 15:28:18 -0500

    Actualizacion Models API

--- chequeo/chequeo.model.js
+++ chequeo/chequeo.model.js
@@ -1,14 +1,17 @@
 const mongoose = require("mongoose");
 
 const schemaChequeo = new mongoose.Schema({
-    IDGanado: {type: String, required: true},
-    // Parametros todavia no definidos
-    isActive: { type: Boolean, required: true, default: true } // Soft Delete
-  }, {
-    versionKey: false,
-    timestamps: true
+  IDGanado: { type: String, required: true },
+  fecha: { type: Date, required: true },
+  peso: { type: String, required: true },
+  estatura: { type: String, required: true },
+  temperaturaCorporal: { type: String, required: true },
+  isActive: { type: Boolean, required: true, default: true } // Soft Delete 
+}, {
+  versionKey: false,
+  timestamps: true
 });
-  
+
 const Chequeo = mongoose.model('Chequeo', schemaChequeo);
 
 module.exports = Chequeo;
